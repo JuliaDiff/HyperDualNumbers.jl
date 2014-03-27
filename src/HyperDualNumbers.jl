3e51da4b1d43d4379779b830ff4ba84fbd889e78
Rob J. Goedman <goedman@mac.com>
2014-03-27 15:56:57 -0700

    Initial version

--- src/HyperDualNumbers.jl
+++ src/HyperDualNumbers.jl
@@ -1,5 +1,22 @@
 module HyperDualNumbers
 
-# package code goes here
+  importall Base
+  
+  include("hyperdual.jl")
+  
+  export
+    Hyper,
+    Hyper64,
+    Hyper32,
+    hyper,
+    hyper64,
+    hyper32,
+    ishyper,
+    hyper_show,
+    eps1,
+    eps2,
+    eps1eps2,
+    abshyper,
+    abs2hyper
 
 end # module

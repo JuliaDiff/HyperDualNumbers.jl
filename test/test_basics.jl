56f45d163bc0620132469900fc56c90f59e2f159
Rob J. Goedman <goedman@mac.com>
2014-04-27 12:37:50 -0700

    Updated runtests.jl and test subdirectory

---
+++ test/test_basics.jl
@@ -0,0 +1,57 @@
+using HyperDualNumbers
+using Base.Test  
+
+# Define a few hyperdual numbers for testing purposes
+hd0 = Hyper()
+hd1 = Hyper(1.0)
+
+hd2 = Hyper(1.0, 2.0, 3.0, 4.0)
+hd3 = hyper(1.0, 3.0, 3.0, 4.0)
+hd4 = hyper256(1, 4, 3, 4)
+hd5 = Hyper256(1//1, 10//2, 6//2, 20//5)
+
+hd6 = hyper128(1.0, 6.0, 3.0, 4.0)
+hd7 = Hyper128(1//1, 10//2, 6//2, 20//5)
+
+hdNaN = hyper(0/0)
+
+# Addition and subtraction
+@test eps1(hd1) == 0.0
+@test hd1 + hd2 == hyper(2.0, 2.0, 3.0, 4.0)
+@test -hd2 == hyper(-1.0, -2.0, -3.0, -4.0)
+@test +hd3 == hd2 + hyper(0.0, 1.0, 0.0, 0.0)
+@test hd1-hd3 == hyper(0.0, -3.0, -3.0, -4.0)
+@test 2+hd2 == hyper256(3.0, 2.0, 3.0, 4.0)
+@test hd4-5 == hyper256(-4.0, 4.0, 3.0, 4.0)
+
+# NaN tests
+#@test hdNaN == hyper(NaN, 0.0, 0.0, 0.0)
+#@test hdNaN + hd4 == hyper128(NaN, 4.0, 3.0, 4.0)
+
+println("\nExamples of show() for hyperdual numbers with NaN:\n")
+println("hdNaN = $(hdNaN)")
+println("hdNaN + hd4 = $(hdNaN + hd4)")
+
+@test isnan(hdNaN) == true
+@test isnan(hdNaN+hd4) == true
+@test eps1eps2(hdNaN+hd4) == 4.0
+
+# Using and mixing 64 & 32 bits
+@test hd6+hd7 == hyper(2.0, 11.0, 6.0, 8.0)
+@test hd7+hd5 == 2//1 * hd7
+
+# Multiplication
+@test hd1 * hd2 == hyper128(1, 2, 3, 4)
+@test hd0*hd2 == hyper()
+
+# Division
+@test 1/(1/hd2) == hd2
+
+# Power of
+@test hd3^3 == hd3 * hd3 * hd3
+@test (hd3^3)^(1/3) == hd3
+
+# Mixing types
+@test hd5*hd7 == hd7^2
+@test 1/hd5 == hd7^-1
+@test (hd2^(1//8)^8)^16777216 == hd2

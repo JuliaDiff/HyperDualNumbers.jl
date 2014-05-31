c15ef8b9f25f4e43eae7676f541d16e7011299ed
Rob Goedman <goedman@mac.com>
2014-05-31 11:36:13 -0700

    Removed REQUIRE, fixed show with NaN

--- src/hyperdual.jl
+++ src/hyperdual.jl
@@ -131,7 +131,7 @@ function hyper_show(io::IO, z::Hyper, compact::Bool)
       print(io, "\u03F51\u03F52")
     end
   else
-    print(io, "dual(", f0, ",", f1, ",", f2, ",", f12, ")")
+    print(io, "Hyper(", f0, ")")
   end
 end
 

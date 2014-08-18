36b4be462420c38f0881f37ea3b0e47bca474ec9
Rob Goedman <goedman@mac.com>
2014-08-18 10:02:38 -0700

    Included Tim's division performance test.

--- test/test_basics.jl
+++ test/test_basics.jl
@@ -45,6 +45,7 @@ println("hdNaN + hd4 = $(hdNaN + hd4)")
 @test hd0*hd2 == hyper()
 
 # Division
+println("\nTesting includes Tim Holy's division performance improvement.")
 @test 1/(1/hd2) == hd2
 @test hd2/hd2 == hd1
 

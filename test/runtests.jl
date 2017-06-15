39436d002d562f27f0277ba817609bbc6491d095
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2017-06-15 14:59:22 +0200

    Added Test for Erf Function using Black Scholes Formula

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 my_tests = ["test_basics.jl",
             "test_TimHoly_example.jl",
-            "test_Paper_example.jl"]
+            "test_Paper_example.jl",
+			"test_Erf_example.jl"]
 
 println("Running tests:")
 

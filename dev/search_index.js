643499870a6d8233dc98da99aeb41239039017bf
autodocs <autodocs>
2018-11-22 19:58:17 +0000

    build based on 4fbbcaa

---
+++ dev/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#HyperDualNumbers-1",
+    "page": "Home",
+    "title": "HyperDualNumbers",
+    "category": "section",
+    "text": "Hyper-dual numbers can be used to compute first and second derivatives numerically without the cancellation errors of finite-differencing schemes. This Julia implementation is directly based on the C++ implementation by Jeffrey Fike and Juan J Alonso, both of Stanford University, department of Aeronautics and Astronautics and is described in the paper:The Development of Hyper-Dual Numbers for Exact Second Derivative CalculationsThe initial Julia-ish versions (up to v3.0.1) were derived/written by Rob J Goedman (goedman@icloud.com).HyperDualNumbers.jl v4.0.0 has been completely redone by Benoit Pasquier to make it Julia and much better follows the structure of the JuliaDiff/DualNumbers package.]()"
+},
+
+{
+    "location": "#Symbolic-derivative-list-1",
+    "page": "Home",
+    "title": "Symbolic derivative list",
+    "category": "section",
+    "text": "HyperDualNumbers.symbolic_derivative_list"
+},
+
+{
+    "location": "#Index-1",
+    "page": "Home",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+]}

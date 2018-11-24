0f4d892dfb9e242250d350c57746f5b1cf38b2dc
autodocs <autodocs>
2018-11-24 21:07:55 +0000

    build based on b0bf9b2

--- dev/search_index.js
+++ dev/search_index.js
@@ -13,7 +13,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "HyperDualNumbers",
     "category": "section",
-    "text": "Hyper-dual numbers can be used to compute first and second derivatives numerically without the cancellation errors of finite-differencing schemes.The initial Julia implementation (and up to v3.0.1) is directly based on the C++ implementation by Jeffrey Fike and Juan J Alonso, both of Stanford University, department of Aeronautics and Astronautics and is described in the paper:The Development of Hyper-Dual Numbers for Exact Second Derivative CalculationsThose initial versions were derived/written by Rob J Goedman (goedman@icloud.com).HyperDualNumbers.jl v4.0.0 has been completely redone by Benoit Pasquier and follows the structure of the JuliaDiff/DualNumbers package.]()"
+    "text": "Hyper-dual numbers can be used to compute first and second derivatives numerically without the cancellation errors of finite-differencing schemes.The initial Julia implementation (and up to v3.0.1) is directly based on the C++ implementation by Jeffrey Fike and Juan J Alonso, both of Stanford University, department of Aeronautics and Astronautics and is described in the paper:The Development of Hyper-Dual Numbers for Exact Second Derivative CalculationsThose initial versions have been derived/written by Rob J Goedman (goedman@icloud.com).HyperDualNumbers.jl v4.0.0 has been completely redone by Benoit Pasquier and follows the structure of the JuliaDiff/DualNumbers package.]CurrentModule = HyperDualNumbers"
+},
+
+{
+    "location": "#Creation-of-a-HyperDualNumber-1",
+    "page": "Home",
+    "title": "Creation of a HyperDualNumber",
+    "category": "section",
+    "text": "Hyper()"
 },
 
 {

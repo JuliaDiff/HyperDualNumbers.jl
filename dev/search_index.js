dbee67144ce802736db2bea0839ba660bf4a9519
autodocs <autodocs>
2018-11-24 21:40:08 +0000

    build based on 26d5104

--- dev/search_index.js
+++ dev/search_index.js
@@ -2,15 +2,15 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "#",
-    "page": "Home",
-    "title": "Home",
+    "page": "HyperDualNumbers",
+    "title": "HyperDualNumbers",
     "category": "page",
     "text": ""
 },
 
 {
     "location": "#HyperDualNumbers-1",
-    "page": "Home",
+    "page": "HyperDualNumbers",
     "title": "HyperDualNumbers",
     "category": "section",
     "text": "Hyper-dual numbers can be used to compute first and second derivatives numerically without the cancellation errors of finite-differencing schemes.The initial Julia implementation (and up to v3.0.1) is directly based on the C++ implementation by Jeffrey Fike and Juan J Alonso, both of Stanford University, department of Aeronautics and Astronautics and is described in the paper:The Development of Hyper-Dual Numbers for Exact Second Derivative CalculationsThe early versions have been derived/written by Rob J Goedman (goedman@icloud.com).HyperDualNumbers.jl v4.0.0 has been completely redone by Benoit Pasquier and follows the structure of the JuliaDiff/DualNumbers package.]CurrentModule = HyperDualNumbers"
@@ -18,7 +18,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "#Creation-of-a-HyperDualNumber-1",
-    "page": "Home",
+    "page": "HyperDualNumbers",
     "title": "Creation of a HyperDualNumber",
     "category": "section",
     "text": "HyperDualNumbers.Hyper()"
@@ -26,7 +26,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "#Symbolic-derivative-list-1",
-    "page": "Home",
+    "page": "HyperDualNumbers",
     "title": "Symbolic derivative list",
     "category": "section",
     "text": "HyperDualNumbers.symbolic_derivative_list"
@@ -34,7 +34,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "#Fields-of-a-HyperDualNumber-1",
-    "page": "Home",
+    "page": "HyperDualNumbers",
     "title": "Fields of a HyperDualNumber",
     "category": "section",
     "text": "HyperDualNumbers.ɛ₁\nHyperDualNumbers.ɛ₂\nHyperDualNumbers.ε₁ɛ₂"
@@ -42,7 +42,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "#Index-1",
-    "page": "Home",
+    "page": "HyperDualNumbers",
     "title": "Index",
     "category": "section",
     "text": ""

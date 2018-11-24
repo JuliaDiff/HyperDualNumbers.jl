26d51044982a1907d61fd5f3df1d200dfa73536a
Rob J Goedman <goedman@mac.com>
2018-11-24 14:37:37 -0700

    Updated make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,20 @@
 using HyperDualNumbers
 using Documenter
 
-makedocs(
-    modules = [HyperDualNumbers],
-    format = :html,
-    checkdocs = :exports,
+const src_path = @__DIR__
+
+"Relative path using the HypoerDualNumbers src/ directory."
+rel_path(parts...) = normpath(joinpath(src_path, parts...))
+
+DOCROOT = rel_path("../docs")
+
+makedocs(root = DOCROOT,
+    modules = Module[],
     sitename = "HyperDualNumbers.jl",
-    pages = Any[
-        "Home" => "index.md"
-    ]
+    pages = Any["index.md"]
 )
 
-deploydocs(
+deploydocs(root = DOCROOT,
     repo = "github.com/JuliaDiff/HyperDualNumbers.jl.git"
  )
+ 
